e2d42550e73ac4c5775955fb4b6afdd019693d4e
xilingwu <wuxiling@outlook.com>
2019-06-30 03:16:08 -0400

    Q3a done

---
+++ a2/Q3/Q3a/comment-script.js
@@ -0,0 +1,19 @@
+function fn(){
+    var path = window.location.pathname.substring(window.location.pathname.lastIndexOf("/")+1);
+    console.log(path);
+    console.log(window.location.search);
+    console.log(window.location.search.match(/\\?id=[0-9]+/));
+    if(path == '/view.php') {
+        var str = window.location.search;
+        var re = new RegExp(/\\?id=[0-9]+/);
+        if(re.test(str)){// contains id
+            console.log("matched");
+            document.getElementById("author").innerHTML = "posted by AGoldmund";
+            var idstr = str.match(re);
+            var id = idstr.substring(idstr.lastIndexOf("=")+1);
+            return id;
+        }
+    }else{
+        return;
+    }
+}
